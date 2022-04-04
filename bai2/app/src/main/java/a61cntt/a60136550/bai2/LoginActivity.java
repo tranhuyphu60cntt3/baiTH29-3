5424308fa97feb1cf23afeb5f12df7081abcaaa2
phu <phu.th.60cntt@ntu.edu.vn>
2022-04-05 02:06:22 +0700

    5/4/22

--- bai2/app/src/main/java/a61cntt/a60136550/bai2/LoginActivity.java
+++ bai2/app/src/main/java/a61cntt/a60136550/bai2/LoginActivity.java
@@ -2,7 +2,12 @@ package a61cntt.a60136550.bai2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button nut = (Button) findViewById(R.id.btnok);
+        nut.setOnClickListener(sangHome);
     }
+    View.OnClickListener sangHome = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            EditText tk = (EditText) findViewById(R.id.edtusername);
+            EditText mk = (EditText) findViewById(R.id.edtpass);
+            String user = "phu";
+            String pass = "123";
+            String t = tk.getText().toString();
+            String m = mk.getText().toString();
+
+            if ((t.equals(user)) && (m.equals(pass))){
+                Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_LONG).show();
+                Intent Home = new Intent(LoginActivity.this, HomeActivity.class);
+                Home.putExtra("user", t);
+                startActivity(Home);
+            }
+            else {
+                Toast.makeText(LoginActivity.this, "Đăng nhập không thành công", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
