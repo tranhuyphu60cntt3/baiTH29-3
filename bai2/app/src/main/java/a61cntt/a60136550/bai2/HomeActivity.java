045f690c965009690a4a1f01662186f3f1e06c11
phu <phu.th.60cntt@ntu.edu.vn>
2022-04-04 22:29:53 +0700

    4/4/2022

---
+++ bai2/app/src/main/java/a61cntt/a60136550/bai2/HomeActivity.java
@@ -0,0 +1,14 @@
+package a61cntt.a60136550.bai2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
