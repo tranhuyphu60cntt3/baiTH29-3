f5be5a402fa9b10c7821f57051cab3f5a6b2877b
phu <phu.th.60cntt@ntu.edu.vn>
2022-04-04 23:48:39 +0700

    4/4/22 lan2

--- bai2/app/src/main/java/a61cntt/a60136550/bai2/MainActivity.java
+++ bai2/app/src/main/java/a61cntt/a60136550/bai2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button nut = (Button) findViewById(R.id.btnlogin);
+        nut.setOnClickListener(sanglogin);
     }
-    public void ChuyenManhinh(View v){
-        Intent iManhinhKhac = new Integer( MainActivity.this, SubActivity.class );
-        startActivity(iManhinhKhac);
-    }
-
+    View.OnClickListener sanglogin = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent Login = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(Login);
+        }
+    };
 }
